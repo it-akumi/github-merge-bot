d1cc605798d82e996715b87739c0ec444bb29384
it_akumi <it.akumi1011@gmail.com>
2018-07-31 17:59:44 +0900

    Enable to parse request

--- github-merge-bot/github-merge-bot.go
+++ github-merge-bot/github-merge-bot.go
@@ -1,14 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"errors"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/google/go-github/github"
 )
 
-func GitHubMergeHandler() {
-	fmt.Println("Hello, github-merge-bot!")
+func HandleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	pr := new(github.PullRequest)
+	if err := json.Unmarshal([]byte(request.Body), &pr); err != nil {
+		return events.APIGatewayProxyResponse{}, errors.New("Failed to unmarshal JSON")
+	}
+
+	// Try to merge PullRequest
+
+	// Notify result to slack
+
+	return events.APIGatewayProxyResponse{StatusCode: 200, Body: "OK"}, nil
 }
 
 func main() {
-	lambda.Start(GitHubMergeHandler)
+	lambda.Start(HandleRequest)
 }

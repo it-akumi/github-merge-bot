3a3561991ddc80566992351fc494ad98e8a6b4c9
it_akumi <it.akumi1011@gmail.com>
2018-08-04 20:41:32 +0900

    Fix slack.Notify
    []byte doesn't implement io.Reader
    
    cannot call pointer method on http.Client literal

--- github-merge-bot/slack/slack.go
+++ github-merge-bot/slack/slack.go
@@ -1,9 +1,9 @@
 package slack
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
-	"net/url"
 	"os"
 )
 
@@ -11,22 +11,28 @@ type Message struct {
 	Text string `json:"text"`
 }
 
-func Notify(msg Message) {
-	req, err := http.NewRequest{
+func Notify(text string) {
+	body, err := json.Marshal(Message{Text: text})
+	if err != nil {
+		println("Failed to marshal message")
+		os.Exit(1)
+	}
+	req, err := http.NewRequest(
 		"POST",
 		os.Getenv("SLACK_INCOMING_WEBHOOK_URL"),
-		json.Mershal(msg),
-	}
+		bytes.NewBuffer(body),
+	)
 	if err != nil {
-		println("Failed to create new request...")
+		println("Failed to create new request")
 		os.Exit(1)
 	}
 
 	req.Header.Set("Content-Type", "application/json")
 
-	resp, err := &http.client{}.Do(req)
+	client := &http.Client{}
+	resp, err := client.Do(req)
 	if err != nil {
-		println("Failed to notify to slack...")
+		println("Failed to notify to slack")
 		os.Exit(1)
 	}
 	defer resp.Body.Close()

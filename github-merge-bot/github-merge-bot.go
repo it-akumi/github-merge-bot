cca0edc6656242f2aaed77982caff2a4beb01ed2
it_akumi <it.akumi1011@gmail.com>
2018-08-07 14:46:15 +0900

    Make HandleRequest return a custom error
    With the Lambda proxy integration, API Gateway requires the backend
    Lambda function to return output according to specified format.
    It returns 502 when the format is different.

--- github-merge-bot/github-merge-bot.go
+++ github-merge-bot/github-merge-bot.go
@@ -4,7 +4,6 @@ import (
 	"./slack"
 	"context"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -38,11 +37,17 @@ func mergePullRequest(owner, repo string, number int) (string, int, error) {
 func HandleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	prEvent := new(github.PullRequestEvent)
 	if err := json.Unmarshal([]byte(request.Body), &prEvent); err != nil {
-		return events.APIGatewayProxyResponse{}, errors.New("Failed to unmarshal JSON")
+		return events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body:       "Failed to unmarshal JSON\nRequest body may be invalid",
+		}, nil
 	}
 
 	if prEvent.GetAction() != "review_requested" {
-		return events.APIGatewayProxyResponse{StatusCode: 400}, errors.New("action must be review_requested")
+		return events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body:       "Request parameter 'action' must be 'review_requested'",
+		}, nil
 	}
 
 	owner := *prEvent.GetRepo().Owner.Login
@@ -52,9 +57,9 @@ func HandleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProx
 	slack.Notify(fmt.Sprint(resultMessage))
 
 	if err != nil {
-		return events.APIGatewayProxyResponse{StatusCode: statusCode}, err
+		return events.APIGatewayProxyResponse{StatusCode: statusCode, Body: resultMessage}, nil
 	}
-	return events.APIGatewayProxyResponse{StatusCode: 200}, nil
+	return events.APIGatewayProxyResponse{StatusCode: 200, Body: resultMessage}, nil
 }
 
 func main() {
